8162873301f8175fd103a6bcd6d7282a294da82a
Calvin Griffin <cgriffin332@gmail.com>
2020-10-01 16:28:55 -0400

    added basic server info to js

---
+++ server.js
@@ -0,0 +1,28 @@
+// ==============================================================================
+// DEPENDENCIES
+
+var express = require("express");
+
+// Tells node that we are creating an "express" server
+var app = express();
+
+// Sets an initial port. 
+var PORT = process.env.PORT || 8080;
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// ================================================================================
+// ROUTER
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+// =============================================================================
+// LISTENER
+
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
