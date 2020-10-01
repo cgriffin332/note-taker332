f50374f13d2971073de53848b2279924b8f0b296
Calvin Griffin <cgriffin332@gmail.com>
2020-10-01 16:39:05 -0400

    simplified route

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -8,16 +8,12 @@ var path = require("path");
 module.exports = function (app) {
   // HTML GET Requests
   //--------------------------------------------------------------------------
-  // go to homepage
-  app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
-  });
   // go to notes page
   app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/notes.html"));
   });
 
-  // If no matching route is found default to home
+  // go to homepage
   app.get("*", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/index.html"));
   });
